0803f84962eec468fa681c18bf3762b62e4c4228
Ripio <ripioconarcilla@gmail.com>
2023-12-17 00:00:09 -0300

    docker and kubernetes configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EngineModule } from './engine/engine.module';
 import { EngineController } from './engine/engine.controller';
 import { EngineService } from './engine/engine.service';
 import { DimProcess } from './entities/dim-process.entity';
 import { DimTransactionType } from './entities/dim-transaction-type.entity';
 import { FactWorkflow } from './entities/fact-workflow.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'sale-management-system',
-      schema: 'engine',
-      synchronize: false,
-      logging: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        schema: 'engine',
+        synchronize: false,
+        logging: true,
+        autoLoadEntities: true,
+      }),
     }),
     TypeOrmModule.forFeature([FactWorkflow, DimProcess, DimTransactionType])
   ],
